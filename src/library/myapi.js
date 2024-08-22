a2b32791d0a77cc1dc7e842873e7cee93aa7722e
Ron Ron Marquez <rsmarquez@sl-agritech.com>
2024-08-22 11:56:41 +0800

    adjustment

--- src/library/myapi.js
+++ src/library/myapi.js
@@ -4,9 +4,11 @@ import { CapacitorHttp } from '@capacitor/core';
 // const API_URL = 'http://localhost/api_spottweb/spott'; // localhost
 // const DB_NAME = 'spottdb';
 
-const API_URL = 'https://spottapp.online/api/spott';
-const DB_NAME = 'if0_37069932_spottdb';
+// const API_URL = 'https://spottapp.online/api/spott';
+// const DB_NAME = 'if0_37069932_spottdb';
 
+const API_URL = 'http://192.168.86.1/api/spott'; // localhost
+const DB_NAME = 'spottdb';
 
 const MyApi = {
     install(app) {
