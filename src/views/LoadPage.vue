01aa9fb893afd0ceb41dabc64a970bc3c0beb9c9
Ron Ron Marquez <rsmarquez@sl-agritech.com>
2024-08-29 08:18:57 +0800

    load page

--- src/views/LoadPage.vue
+++ src/views/LoadPage.vue
@@ -11,20 +11,43 @@ import { IonPage, IonSpinner } from '@ionic/vue';
 export default {
   components: { IonPage, IonSpinner },
   async created(){
-    try {
-      const user = await this.$storage.getItem('session-user');
-      if(user == null){
-        setTimeout(() => { this.$router.push('login') }, 500);
+    const user = await this.$storage.getItem('session-user');
+    const net = await Network.getStatus();
+    const appVersion = '1.2.4';
+
+    if(net.connectionType != 'none'){
+      const res = await this.$api.getappconfig();
+      console.log('Current Ver: ' + appVersion + ' | Latest Ver: ' + res.version);
+      if (res.version != appVersion) {
+        this.$router.push('update');
       }else{
-        setTimeout(() => { this.$router.push('dashboard') }, 500);
+        if(user){
+          this.$router.push('dashboard');
+        }else{
+          this.$router.push('login');
+        }
       }
-    } catch (error) {
-      console.log(error)
-      setTimeout(() => {
-        this.$router.go()
-      }, 500);
+    }else{
+      if(user){
+        this.$router.push('dashboard');
+      }else{
+        this.$router.push('login');
+      }
     }
-    
+
+    // try {
+    //   const user = await this.$storage.getItem('session-user');
+    //   if(user == null){
+    //     setTimeout(() => { this.$router.push('login') }, 500);
+    //   }else{
+    //     setTimeout(() => { this.$router.push('dashboard') }, 500);
+    //   }
+    // } catch (error) {
+    //   console.log(error)
+    //   setTimeout(() => {
+    //     this.$router.go()
+    //   }, 500);
+    // }
   }
 }
 </script>
